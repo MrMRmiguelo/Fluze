e0b391c026328515c6b501bae0a04f86d0790c1e
MrMRmiguelo <miguelovaldez01@gmail.com>
2019-11-17 19:06:02 -0600

    Adicion de clases

---
+++ app/src/main/java/com/example/fluze/ui/popular_movies/popular/PopularMoviePageListAdapter.kt
@@ -0,0 +1,148 @@
+package com.example.fluze.ui.popular_movies.popular
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.paging.PagedListAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.fluze.R
+import com.example.fluze.data.apiResponse.POSTER_BASE_URL
+import com.example.fluze.data.repository.NetworkState
+import com.example.fluze.data.vo.Result
+import com.example.fluze.ui.popular_movies.SingleMovie.SingleMovie
+import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.activity_single_movie.view.*
+import kotlinx.android.synthetic.main.movie_list_tem.view.*
+
+class PopularMoviePageListAdapter (public val context: Context) : PagedListAdapter<Result, RecyclerView.ViewHolder>(MovieDiffCallback()) {
+    val MOVIE_VIEW_TYPE = 1
+    val NETWORK_VIEW_TYPE = 2
+
+    private var networkState: NetworkState? = null
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view: View
+
+        if (viewType == MOVIE_VIEW_TYPE) {
+            view = layoutInflater.inflate(R.layout.movie_list_item, parent, false)
+            return MovieItemViewHolder(view)
+        } else {
+            view = layoutInflater.inflate(R.layout.network_state_item, parent, false)
+            return NetworkStateItemViewHolder(view)
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (getItemViewType(position) == MOVIE_VIEW_TYPE) {
+            (holder as MovieItemViewHolder).bind(getItem(position),context)
+        }
+        else {
+            (holder as NetworkStateItemViewHolder).bind(networkState)
+        }
+    }
+
+
+    private fun hasExtraRow(): Boolean {
+        return networkState != null && networkState != NetworkState.LOADED
+    }
+
+    override fun getItemCount(): Int {
+        return super.getItemCount() + if (hasExtraRow()) 1 else 0
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (hasExtraRow() && position == itemCount - 1) {
+            NETWORK_VIEW_TYPE
+        } else {
+            MOVIE_VIEW_TYPE
+        }
+    }
+
+
+
+
+    class MovieDiffCallback : DiffUtil.ItemCallback<Result>() {
+        override fun areItemsTheSame(oldItem: Result, newItem: Result): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Result, newItem: Result): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+
+    class MovieItemViewHolder (view: View) : RecyclerView.ViewHolder(view) {
+
+        fun bind(movie: Result?,context: Context) {
+            itemView.cv_movie_title.text = movie?.title
+            itemView.cv_movie_release_date.text =  movie?.releaseDate
+
+            val moviePosterURL = POSTER_BASE_URL + movie?.posterPath
+            Glide.with(itemView.context)
+                .load(moviePosterURL)
+                .into(itemView.cv_iv_movie_poster);
+
+            itemView.setOnClickListener{
+                val intent = Intent(context, SingleMovie::class.java)
+                intent.putExtra("id", movie?.id)
+                context.startActivity(intent)
+            }
+
+        }
+
+    }
+
+    class NetworkStateItemViewHolder (view: View) : RecyclerView.ViewHolder(view) {
+
+        fun bind(networkState: NetworkState?) {
+            if (networkState != null && networkState == NetworkState.LOADING) {
+                itemView.progress_bar.visibility = View.VISIBLE;
+            }
+            else  {
+                itemView.progress_bar.visibility = View.GONE;
+            }
+
+            if (networkState != null && networkState == NetworkState.ERROR) {
+                itemView.txt_error.visibility = View.VISIBLE;
+                itemView.txt_error.text = networkState.msg;
+            }
+            else if (networkState != null && networkState == NetworkState.ENDOFLIST) {
+                itemView.txt_error.visibility = View.VISIBLE;
+                itemView.txt_error.text = networkState.msg;
+            }
+            else {
+                itemView.txt_error.visibility = View.GONE;
+            }
+        }
+    }
+
+
+    fun setNetworkState(newNetworkState: NetworkState) {
+        val previousState = this.networkState
+        val hadExtraRow = hasExtraRow()
+        this.networkState = newNetworkState
+        val hasExtraRow = hasExtraRow()
+
+        if (hadExtraRow != hasExtraRow) {
+            if (hadExtraRow) {
+                notifyItemRemoved(super.getItemCount())
+            } else {
+                notifyItemInserted(super.getItemCount())
+            }
+        } else if (hasExtraRow && previousState != newNetworkState) {
+            notifyItemChanged(itemCount - 1)
+        }
+
+    }
+
+}
+
+
